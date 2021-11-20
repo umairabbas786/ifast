bc0b699f14c209a990de1372bb94152af9bdb7a3
Umair Abbas <umairabbass786@gmail.com>
2021-11-20 21:18:45 +0500

    small changes

--- include/footer.php
+++ include/footer.php
@@ -5,7 +5,7 @@
             <script>
               document.write(new Date().getFullYear())
             </script> made with <i class="material-icons" style="font-size:16px;">favorite</i> by
-            <a href="#" target="_blank">Wolf Software Solutions</a> (PTV LTD)
+            <a href="#" target="_blank">Light House</a>
           </div>
         </div>
       </footer>
